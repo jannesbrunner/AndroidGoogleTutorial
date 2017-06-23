6c03e827572af080882a4237992ee1285c31ec2c
jannesbrunner <mail@jannebrunner.de>
2017-06-23 15:26:32 +0200

    Display the message
    - get the Intent that started the activity and extract the string
    - capture the layout's TextView and set the string as its text

--- app/src/main/java/jannes/example/com/googletutorial/DisplayMessageActivity.java
+++ app/src/main/java/jannes/example/com/googletutorial/DisplayMessageActivity.java
@@ -1,7 +1,9 @@
 package jannes.example.com.googletutorial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        // get the Intent that started the activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
